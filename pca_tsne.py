31724e718dd460a2095ee1dd2afe2b8c0e61b27b
Supreet Kaur <77117421+KaurSupreet05@users.noreply.github.com>
2022-05-13 13:31:43 -0500

    Update pca_tsne.py

--- pca_tsne.py
+++ pca_tsne.py
@@ -34,6 +34,10 @@ tsne = np.transpose(TSNE(n_components = 2, perplexity=30).fit_transform(pca)) #h
 eigenvalues = n20.explained_variance_ratio_
 print(eigenvalues)
 
+np.savetxt('pca-wr-2d.out',pca,delimiter=' ')
+np.savetxt('tsne-wr-2d.out',tsne,delimiter=' ')
+
+
 # ORDER PARAMETER CALCULATION (mean and standard deviation)
 
 mean = np.zeros((ndens,2))
@@ -49,20 +53,12 @@ for i in range(ndens):
 np.savetxt('mean-pca.out',mean,delimiter=' ')
 np.savetxt('std-dev-pca.out',std,delimiter=' ')
 
-
-# PLOTTING THE SCATTER PLOTS AND ORDER PARAMETER
+# PLOTTING THE ORDER PARAMETER
  # setting the range of color bar data 
-scat_color = np.zeros(ndens*nconfigs)
 sc_color = np.zeros(ndens)
 for i in range(ndens):
-    scat_color[i*nconfigs:(i+1)*nconfigs] = 0.5+0.01*i
     sc_color[i] = 0.5+0.01*i
  # plotting
-fig,axs = plt.subplots(1,2,figsize=(30,15))
-pcm1 = axs[0].scatter(pca[:,0],pca[:,1],c=scat_color,cmap='rainbow')
-pcm2 = axs[1].scatter(tsne[0,:],tsne[1,:],c=scat_color,cmap='rainbow')
-cbar1 = fig.colorbar(pcm1,ax=axs)
-cbar1.set_label('Density',fontsize=30)
 
 fig,axs = plt.subplots(1,2,figsize=(30,15))
 pcm3 = axs[0].scatter(mean[:,0],mean[:,1],c=sc_color,cmap='rainbow')
